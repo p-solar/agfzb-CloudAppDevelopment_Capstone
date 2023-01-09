8ffb34d420e5b3fbe34d5e43f2f5c3fcf918ec24
Prasanna Rotti <protti@enphaseenergy.com>
2023-01-08 19:59:34 -0800

    added login/logout and registeraton

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,11 +15,15 @@ urlpatterns = [
 
     # path for contact us view
 
-    # path for registration
-
+     # path for registration
+    path(route='registration/', view=views.registration_request, name='registration'),
+    
     # path for login
-
+    path(route='login/', view=views.login_request, name='login'),
+    
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
+
 
     path(route='', view=views.get_dealerships, name='index'),
 
