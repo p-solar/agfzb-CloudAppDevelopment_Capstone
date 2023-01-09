988bb3d1671e0bf0c9eb6fdd5f005317a78ffa7c
Prasanna Rotti <protti@enphaseenergy.com>
2023-01-08 23:16:23 -0800

    addded get dealer

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -1,21 +1,70 @@
-/**
- * Get all dealerships
- */
-
-const { CloudantV1 } = require('@ibm-cloud/cloudant');
-const { IamAuthenticator } = require('ibm-cloud-sdk-core');
-
-async function main(params) {
-      const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+function main(params) {
+  // console.log(params);
+  return new Promise(function (resolve, reject) {
+      const { CloudantV1 } = require('@ibm-cloud/cloudant');
+      const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+      const authenticator = new IamAuthenticator({ apikey: params.API_KEY })
       const cloudant = CloudantV1.newInstance({
           authenticator: authenticator
       });
-      cloudant.setServiceUrl(params.COUCH_URL);
-      try {
-        let dbList = await cloudant.getAllDbs();
-        return { "dbs": dbList.result };
-      } catch (error) {
-          return { error: error.description };
-      }
-}
-
+      cloudant.setServiceUrl(params.URL);
+      if (params.st) {
+          // return dealership with this state 
+          cloudant.postFind({db:'dealerships',selector:{st:params.st}})
+          .then((result)=>{
+            // console.log(result.result.docs);
+            let code = 200;
+            if (result.result.docs.length == 0) {
+                code = 404;
+            }
+            resolve({
+                statusCode: code,
+                headers: { 'Content-Type': 'application/json' },
+                body: result.result.docs
+            });
+          }).catch((err)=>{
+            reject(err);
+          })
+      } else if (params.id) {
+          id = parseInt(params.dealerId)
+          // return dealership with this state 
+          cloudant.postFind({
+            db: 'dealerships',
+            selector: {
+              id: parseInt(params.id)
+            }
+          })
+          .then((result)=>{
+            // console.log(result.result.docs);
+            let code = 200;
+            if (result.result.docs.length == 0) {
+                code = 404;
+            }
+            resolve({
+                statusCode: code,
+                headers: { 'Content-Type': 'application/json' },
+                body: result.result.docs
+            });
+          }).catch((err)=>{
+            reject(err);
+          })
+      } else {
+          // return all documents 
+          cloudant.postAllDocs({ db: 'dealerships', includeDocs: true, limit: 10 })            
+          .then((result)=>{
+            // console.log(result.result.rows);
+            let code = 200;
+            if (result.result.rows.length == 0) {
+                code = 404;
+            }
+            resolve({
+                statusCode: code,
+                headers: { 'Content-Type': 'application/json' },
+                body: result.result.rows
+            });
+          }).catch((err)=>{
+            reject(err);
+          })
+    }
+  }
+  )}
